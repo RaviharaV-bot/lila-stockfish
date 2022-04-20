3df9dad3b2b208bc66e73337782c144d3ca819fa
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-04-20 17:04:27 +0530

    Update engine_wrapper.py

--- engine_wrapper.py
+++ engine_wrapper.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 
 
 @backoff.on_exception(backoff.expo, BaseException, max_time=120)
-def create_engine(config):
+def create_engine(config, variant):
     cfg = config["engine"]
-    engine_path = os.path.join(cfg["dir"], cfg["name"])
+    engine_path = os.path.join(cfg["dir"], cfg.get(variant) or cfg["name"])
     engine_working_dir = cfg.get("working_dir") or os.getcwd()
     engine_type = cfg.get("protocol")
     engine_options = cfg.get("engine_options")
